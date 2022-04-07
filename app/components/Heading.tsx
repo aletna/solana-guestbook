50204975a21c730bbc14f28226d660fd516adcce
aletna <dennis.antela@gmail.com>
2022-04-07 15:46:54 -0700

    no wall additions on mobile

--- app/components/Heading.tsx
+++ app/components/Heading.tsx
@@ -8,34 +8,36 @@ type Props = {
 };
 const Heading = ({ wallet, setShowInput, showInput }: Props) => {
   return (
-    <div className="flex justify-between items-center mt-4 ">
+    <div className="flex justify-between items-center mt-4 p-1 ">
       <div className="text-3xl py-3 text-[#f2e9e4] font-bold ">
         guestbook (devnet)
       </div>
-      {(!wallet || !wallet.publicKey) && (
-        <WalletMultiButton
-          style={{ backgroundColor: "#f2e9e4 !important" }}
-          startIcon={null as any}
-        >
-          <span
-            style={{
-              borderRadius: "4px",
-            }}
+      <div className="hidden md:block">
+        {(!wallet || !wallet.publicKey) && (
+          <WalletMultiButton
+            style={{ backgroundColor: "#f2e9e4 !important" }}
+            startIcon={null as any}
           >
-            Connect Wallet
-          </span>
-        </WalletMultiButton>
-      )}
-      {wallet && wallet.publicKey && !showInput && (
-        <div className="text-right">
-          <button
-            className="px-6 bg-[#f2e9e4] text-[#22223b] py-3 border-2 border-black --shadow hover:bg-[#c9ada7] transition ease-in-out duration-200 rounded-lg "
-            onClick={() => setShowInput(true)}
-          >
-            leave message
-          </button>
-        </div>
-      )}
+            <span
+              style={{
+                borderRadius: "4px",
+              }}
+            >
+              Connect Wallet
+            </span>
+          </WalletMultiButton>
+        )}
+        {wallet && wallet.publicKey && !showInput && (
+          <div className="text-right">
+            <button
+              className="px-6 bg-[#f2e9e4] text-[#22223b] py-3 border-2 border-black --shadow hover:bg-[#c9ada7] transition ease-in-out duration-200 rounded-lg "
+              onClick={() => setShowInput(true)}
+            >
+              leave message
+            </button>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
